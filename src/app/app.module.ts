17f06d405e9cbfa290f5cb108e3b08a1864bafba
Arulmozhimanikandan <arulmozhimanikandan@Arulmozhimanikandans-MacBook-Pro.local>
2019-12-20 16:24:09 +0100

    base header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SkillsComponent } from './skills/skills.component';
+import { BorderComponent } from './border/border.component';
+import { SkillComponent } from './skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SkillsComponent,
+    BorderComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule

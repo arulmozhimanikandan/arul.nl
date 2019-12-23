ae34e778c4a1af8f5448eb4b2acdab725b06cd09
Arulmozhimanikandan <arulmozhimanikandan@Arulmozhimanikandans-MacBook-Pro.local>
2019-12-24 00:19:40 +0100

    add panel left right navigation

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -8,12 +8,40 @@ import {Component, Input, OnInit} from '@angular/core';
 export class PanelComponent implements OnInit {
 
   @Input() experience: {
-    projects: [];
+    projects: any[];
     name: string;
   };
-  constructor() { }
+  selectedProjectIndex = 0;
+  selectedProject: {
+    Description: string;
+    name: string;
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
+    console.log(this.experience);
+    this.selectedProject = this.experience.projects[0];
+  }
+
+  nextProject() {
+    this.selectedProjectIndex++;
+    if (this.selectedProjectIndex < this.experience.projects.length) {
+      this.selectedProject = this.experience.projects[this.selectedProjectIndex];
+    } else {
+      this.selectedProjectIndex --;
+    }
+
+  }
+
+  previousProject() {
+    this.selectedProjectIndex--;
+    if (this.selectedProjectIndex >= 0) {
+      this.selectedProject = this.experience.projects[this.selectedProjectIndex];
+    } else {
+      this.selectedProjectIndex ++;
+    }
   }
 
 }

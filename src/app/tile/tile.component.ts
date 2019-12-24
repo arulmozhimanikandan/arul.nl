7f0ab4356ca5deb83c7f8ed11d6c75d852a65777
Arul mozhi <arulmozhi@Aruls-MacBook-Pro.local>
2019-12-24 14:25:57 +0100

    change selected employer state

--- src/app/tile/tile.component.ts
+++ src/app/tile/tile.component.ts
@@ -7,6 +7,7 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class TileComponent implements OnInit {
 
+  @Input() selected: boolean;
   @Input() tile : {
     initial: string;
   }
